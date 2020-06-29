714f24ad65fa677c21436cfb6b3478f5509403ce
ungmo2 <ungmo2@gmail.com>
2020-06-29 10:16:27 +0900

    enhance render

--- 2_ajax/js/index.js
+++ 2_ajax/js/index.js
@@ -2,57 +2,43 @@
   const root = document.querySelector('.app-root');
   const navigation = document.getElementById('navigation');
 
-  function render(data) {
-    const json = JSON.parse(data);
-    root.innerHTML = `<h1>${json.title}</h1><p>${json.content}</p>`;
-  }
-
-  function renderHtml(html) {
-    root.innerHTML = html;
-  }
-
-  function get(url) {
-    return new Promise((resolve, reject) => {
-      const req = new XMLHttpRequest();
-      req.open('GET', url);
-      req.send();
-
-      req.onreadystatechange = function () {
-        if (req.readyState === XMLHttpRequest.DONE) {
-          if (req.status === 200) resolve(req.response);
-          else reject(req.statusText);
-        }
-      };
-    });
-  }
-
   const routes = {
-    'home': function () {
-      get('/data/home.json').then(render);
-    },
-    'service': function () {
-      get('/data/service.json').then(render);
-    },
-    'about': function () {
-      get('/data/about.html').then(renderHtml);
-    },
-    otherwise(page) {
-      root.innerHTML = `${page} Not Found`;
+    // id: url
+    home: '/data/home.json',
+    service: '/data/service.json',
+    about: '/data/about.html'
+  };
+
+  const render = async id => {
+    try {
+      const url = routes[id];
+      if (!url) {
+        root.innerHTML = `${url} Not Found`;
+        return;
+      }
+
+      const res = await fetch(url);
+      const contentType = res.headers.get('content-type');
+
+      if (contentType?.includes('application/json')) {
+        const json = await res.json();
+        root.innerHTML = `<h1>${json.title}</h1><p>${json.content}</p>`;
+      } else {
+        root.innerHTML = await res.text();
+      }
+    } catch (err) {
+      console.error(err);
     }
   };
 
-  function router(page) {
-    (routes[page] || routes.otherwise)(page);
-  }
-
   // AJAX 요청은 주소창의 url을 변경시키지 않으므로 history 관리가 되지 않는다.
-  navigation.addEventListener('click', e => {
-    if (!e.target || e.target.nodeName !== 'A') return;
+  navigation.onclick = e => {
+    if (!e.target.matches('#navigation > li > a')) return;
     e.preventDefault();
-    router(e.target.id);
-  });
+    render(e.target.id);
+  };
 
   // DOMContentLoaded은 HTML과 script가 로드된 시점에 발생하는 이벤트로 load 이벤트보다 먼저 발생한다. (IE 9 이상 지원)
   // 새로고침이 클릭되었을 때, 웹페이지가 처음 로딩되었을 때, 현 페이지(예를들어 loclahost:5002)를 서버에 요청한다. 이때 Home에 필요한 리소스를 Ajax 요청한다.
-  window.addEventListener('DOMContentLoaded', () => router('home'));
+  window.addEventListener('DOMContentLoaded', () => render('home'));
 }());
--- 4_pjax/js/index.js
+++ 4_pjax/js/index.js
@@ -2,81 +2,63 @@
   const root = document.querySelector('.app-root');
   const navigation = document.getElementById('navigation');
 
-  function render(data) {
-    const json = JSON.parse(data);
-    console.log(json);
-    root.innerHTML = `<h1>${json.title}</h1><p>${json.content}</p>`;
-  }
-
-  function renderHtml(html) {
-    root.innerHTML = html;
-  }
-
-  function get(url) {
-    return new Promise((resolve, reject) => {
-      const req = new XMLHttpRequest();
-      req.open('GET', url);
-      req.send();
-
-      req.onreadystatechange = function () {
-        if (req.readyState === XMLHttpRequest.DONE) {
-          if (req.status === 200) resolve(req.response);
-          else reject(req.statusText);
-        }
-      };
-    });
-  }
-
   const routes = {
-    '/': function () {
-      get('/data/home.json').then(render);
-    },
-    '/service': function () {
-      get('/data/service.json').then(render);
-    },
-    '/about': function () {
-      get('/data/about.html').then(renderHtml);
-    },
-    otherwise(path) {
-      root.innerHTML = `${path} Not Found`;
+    // path: url
+    '/': '/data/home.json',
+    '/service': '/data/service.json',
+    '/about': '/data/about.html'
+  };
+
+  const render = async path => {
+    try {
+      const url = routes[path];
+      if (!url) {
+        root.innerHTML = `${path} Not Found`;
+        return;
+      }
+
+      const res = await fetch(url);
+      const contentType = res.headers.get('content-type');
+
+      if (contentType?.includes('application/json')) {
+        const json = await res.json();
+        root.innerHTML = `<h1>${json.title}</h1><p>${json.content}</p>`;
+      } else {
+        root.innerHTML = await res.text();
+      }
+    } catch (err) {
+      console.error(err);
     }
   };
 
-  function router(path) {
-    (routes[path] || routes.otherwise)(path);
-  }
-
-  // history entry가 변경되면 발생하는 이벤트
+  // popstate 이벤트는 history entry가 변경되면 발생한다.
   // PJAX 방식은 hash를 사용하지 않으므로 hashchange 이벤트를 사용할 수 없다.
-  // popstate event는 pushState에 의해 발생시키지 않는다.
+  // popstate 이벤트는 pushState에 의해 발생하지 않는다.
   // 이전페이지 / 다음페이지 button 또는 history.back() / history.go(n)에 의해 발생한다.
   window.addEventListener('popstate', e => {
-    // e.state는 pushState 메소드의 첫번째 인수
+    // e.state는 pushState 메서드의 첫번째 인수
     console.log('[popstate]', e.state);
-    // 이전페이지 / 다음페이지 button이 클릭되면 router를 호출
-    router(e.state.path);
+    // 이전페이지 / 다음페이지 button이 클릭되면 render를 호출
+    render(e.state.path);
   });
 
-  // 네비게이션을 클릭하면 주소창의 url이 변경되므로 서버로 요청이 전송된다.
-  // preventDefault를 사용하여 이를 방지하고 history 관리를 위한 처리를 실시
+  // 네비게이션을 클릭하면 주소창의 url이 변경되므로 HTTP 요청이 서버로 전송된다.
+  // preventDefault를 사용하여 이를 방지하고 history 관리를 위한 처리를 실시한다.
   navigation.addEventListener('click', e => {
-    if (!e.target || e.target.nodeName !== 'A') return;
+    if (!e.target.matches('#navigation > li > a')) return;
     e.preventDefault();
     // 이동 페이지
     const path = e.target.getAttribute('href');
 
-    // 주소창의 url은 변경되지만 요청하지는 않는다.
+    // 주소창의 url은 변경되지만 HTTP 요청이 서버로 전송되지는 않는다.
     history.pushState({ path }, null, path);
     // path에 의한 AJAX 요청
-    router(path);
+    render(path);
   });
 
   // 웹페이지가 처음 로딩되었을 때
-  router('/');
+  render('/');
 
-  // DOMContentLoaded은 HTML과 script가 로드된 시점에 발생하는 이벤트로 load 이벤트보다 먼저 발생한다. (IE 9 이상 지원)
-  // window.addEventListener('DOMContentLoaded', router);
-
-  // 새로고침이 클릭되었을 때, 현 페이지(예를들어 loclahost:5004/service)가 서버에 요청된다.
-  // 서버측에는 이에 응답하는 기능이 추가되어야 한다.
+  // 새로고침이 클릭되었을 때, 현 페이지(예를 들어 loclahost:5004/service)가 서버에 요청된다.
+  // 이에 응답하는 기능이 서버 측에 추가되어야 한다.
 }());
